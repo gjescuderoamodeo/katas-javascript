1ccd9d50960aa59d7e65b0d3c5e902985b28229d
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-09-28 21:12:44 +0200

    Update 6.5-kyu.js

--- Katas01/6.5-kyu/6.5-kyu.js
+++ Katas01/6.5-kyu/6.5-kyu.js
@@ -1,9 +1,34 @@
 
 
 function getSumNodes(tree) {
+    if (!tree || !tree.value) {
+        return 0;
+    }
+
+    return tree.value + getSumNodes(tree.left) + getSumNodes(tree.right)
 }
 
 function getNoldorByMurdered(noldorRoyalHouse, isMurdered) {
+    if (noldorRoyalHouse == null || typeof isMurdered != "boolean") {
+        return [];
+    }
+
+    if (!noldorRoyalHouse || !noldorRoyalHouse.value) {
+        return;
+    }
+
+    deathToTheElves = [];
+
+
 }
+
+function pickDeathElves(noldorRoyalHouse) {
+    if (!noldorRoyalHouse || !noldorRoyalHouse.value) {
+        return;
+    }
+
+    return tree.value + getSumNodes(tree.left) + getSumNodes(tree.right)
+}
+
 
 module.exports = { getNoldorByMurdered, getSumNodes };
