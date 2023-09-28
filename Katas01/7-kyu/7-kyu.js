9c6c3d8d2d802cb9dadecad6fc9c9ca72f3b27da
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-09-28 18:20:29 +0200

    Update 7-kyu.js

--- Katas01/7-kyu/7-kyu.js
+++ Katas01/7-kyu/7-kyu.js
@@ -1,12 +1,50 @@
-function getAVG() {
+function getAVG(arr) {
+    if (!Array.isArray(arr) || arr === null) {
+        return 0;
+    }
+    let newArray = arr.filter((valor) => typeof valor === 'number');
 
+    if (newArray.length === 0) {
+        return 0;
+    }
+    let suma = newArray.reduce((acumulador, valor) => acumulador + valor, 0);
+
+    return Math.ceil(suma / newArray.length);
+}
+
+
+function replaceVocalWithPosition(phrase) {
+    if (typeof phrase != 'string' || phrase === null) {
+        return null;
+    }
+
+    let vocals = ["a", "e", "i", "o", "u"];
+    let phrase2 = phrase.toLowerCase();
+    let nuevaFrase = '';
+
+    for (let i = 0; i < phrase.length; i++) {
+        if (vocals.includes(phrase2[i])) {
+            nuevaFrase += i + 1;
+        } else {
+            nuevaFrase += phrase[i];
+        }
+    }
+
+    return nuevaFrase;
 
 }
 
-function replaceVocalWithPosition() {
-}
+function convertArrayStringsToArrayNumbers(array) {
+    if (!Array.isArray(array)) {
+        return [0];
+    }
 
-function convertArrayStringsToArrayNumbers() {
+    let numbers = array.map((element) => {
+        let number = parseFloat(element);
+        return isNaN(number) ? 0 : number;
+    });
+
+    return numbers;
 }
 
 function removeValues() {
